264c1eef89a30d08d1222e142c3f79b8cea4d3ce
fashionzzZ <xuefa921@163.com>
2021-01-26 15:56:13 +0800

    [什么值得买]修改代码

--- README.md
+++ README.md
@@ -9,4 +9,5 @@
 | v2ex     | [v2ex.js](./v2ex.js)   | V2EXCK - v2ex的cookie | 🟢    |
 | 百度贴吧 | [baidu.js](./baidu.js) | BDCK - 百度的cookie  | 🟢    |
 | 京东 | [jd.js](./jd.js) | JD_COOKIE - 京东的cookie | 🟢 |
+| 什么值得买 | [smzdm.js](./smzdm.js) | SMZDM_COOKIE - 什么值得买的cookie | 🟢 |
 
--- smzdm.js
+++ smzdm.js
@@ -21,9 +21,8 @@ const header = {
 function sign() {
     return new Promise(async (resolve) => {
         let data;
+        let url = 'https://zhiyou.smzdm.com/user/checkin/jsonp_checkin';
         try {
-            // let url = `https://zhiyou.smzdm.com/user/checkin/jsonp_checkin?callback=jQuery112403621672099300297_${new Date().getTime()}&_=${new Date().getTime()}`;
-            let url = 'https://zhiyou.smzdm.com/user/checkin/jsonp_checkin';
             let res = await axios.get(url, header);
             if (res.data.error_code === 0) {
                 data = `签到成功!\n签到天数: ${res.data.data.checkin_num} | Lv:${res.data.data.rank} | 经验值:${res.data.data.exp}`
