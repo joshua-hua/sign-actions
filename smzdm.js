5c5eb5b976011e5f082efb411f947efd62353bac
fashionzzZ <xuefa921@163.com>
2021-01-26 15:29:05 +0800

    [什么值得买]修改代码

--- smzdm.js
+++ smzdm.js
@@ -5,6 +5,7 @@ const cookie = process.env.SMZDM_COOKIE;
 const header = {
     headers: {
         Referer: "https://www.smzdm.com/",
+        Host: "zhiyou.smzdm.com",
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36",
         cookie: `'${cookie}'`
     },
@@ -14,7 +15,7 @@ function sign() {
     return new Promise(async (resolve) => {
         let data;
         try {
-            let url = 'https://zhiyou.smzdm.com/user/checkin/jsonp_checkin?callback=&_=';
+            let url = `https://zhiyou.smzdm.com/user/checkin/jsonp_checkin?callback=jQuery112403621672099300297_${new Date().getTime()}&_=${new Date().getTime()}`;
             let res = await axios.get(url, header);
             console.log(res)
             if (res.data.error_code === 0) {
